ca6f1a987eb4c5608827d9f792cc0cfd0bcd83c5
Janne Tuhkanen <janne.tuhkanen@hotmail.com>
2022-04-26 08:29:04 +0300

    Working simple speechbubble server.

--- SpeechBubbleServer/Program.cs
+++ SpeechBubbleServer/Program.cs
@@ -1,47 +1,87 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace SpeechBubbleServer
 {
     class Program
     {
+        static readonly object _lock = new object();
+        static readonly Dictionary<int, TcpClient> list_clients = new Dictionary<int, TcpClient>();
+
         static void Main(string[] args)
         {
-            TcpListener listener = new TcpListener(IPAddress.Any, 1302);
-            listener.Start();
-            
+            int count = 1;
+
+            TcpListener ServerSocket = new TcpListener(IPAddress.Any, 5000);
+            ServerSocket.Start();
+
             while (true)
             {
-                TcpClient client = listener.AcceptTcpClient();
+                TcpClient client = ServerSocket.AcceptTcpClient();
+                lock (_lock) list_clients.Add(count, client);
+                Console.WriteLine("Someone connected");
+
+                Thread thread = new Thread(HandleClients);
+                thread.Start(count);
+                count++;
+            }
+        }
+
+        public static void HandleClients(object o)
+        {
+            int id = (int)o;
+            TcpClient client;
+
+            lock (_lock) client = list_clients[id];
+
+            while (true)
+            {
                 NetworkStream stream = client.GetStream();
-                StreamReader sr = new StreamReader(client.GetStream());
-                StreamWriter sw = new StreamWriter(client.GetStream());
+                byte[] buffer = new byte[1024];
+                int byte_count = 0;
 
                 try
                 {
-                    byte[] buffer = new byte[1024];
-                    stream.Read(buffer, 0, buffer.Length);
-                    int recv = 0;
-                    foreach (byte b in buffer)
-                    {
-                        if (b != 0)
-                        {
-                            recv++;
-                        }
-                    }
-                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
-                    Console.WriteLine("request received");
-                    sw.WriteLine("You rock!");
-                    sw.Flush();
+                    byte_count = stream.Read(buffer, 0, buffer.Length);
                 }
-                
-                catch (Exception e)
+                catch
                 {
-                    Console.WriteLine("Something went wrong.");
-                    sw.WriteLine(e.ToString());
+                }
+
+                if (byte_count == 0)
+                {
+                    break;
+                }
+
+                string data = Encoding.ASCII.GetString(buffer, 0, byte_count);
+                Broadcast(data);
+                Console.WriteLine(data);
+            }
+
+            lock (_lock) list_clients.Remove(id);
+
+            Console.WriteLine("Someone disconnected");
+            client.Client.Shutdown(SocketShutdown.Both);
+            client.Close();
+        }
+
+        public static void Broadcast(string data)
+        {
+            byte[] buffer = Encoding.ASCII.GetBytes(data + Environment.NewLine);
+
+            lock (_lock)
+            {
+                foreach (TcpClient c in list_clients.Values)
+                {
+                    NetworkStream stream = c.GetStream();
+
+                    stream.Write(buffer, 0, buffer.Length);
                 }
             }
         }
