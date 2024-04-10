352866383340ec5a77217f85ff4865b7aff95815
JanneTuhkanen <janne.tuhkanen@solita.fi>
2024-04-10 17:52:55 +0300

    Removed old test

--- SpeechBubble.Server/Program.cs
+++ SpeechBubble.Server/Program.cs
@@ -27,7 +27,7 @@ builder.Services.AddResponseCompression(options =>
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme) .AddCookie();
 
 // Database
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=DESKTOP-5GKRTTK\\SQLEXPRESS;Database=Chat;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True"));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=GM05NRB0\\SQLEXPRESS;Database=Chat;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=True"));
 builder.Services.AddTransient<IAuthenticationService, AuthenticationService>();
 builder.Services.AddIdentity<User, IdentityRole>(options =>
 {
